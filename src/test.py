e1b7bd43af4ac63af6f7b9c8138251cf9cc28883
tasniamaha <tasniamaha17march@gmail.com>
2025-10-28 12:54:00 +0600

    Initial setup

---
+++ src/test.py
@@ -0,0 +1,8 @@
+import torch, numpy, pandas, cv2, streamlit
+
+print("Torch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("NumPy version:", numpy.__version__)
+print("Pandas version:", pandas.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Streamlit version:", streamlit.__version__)
